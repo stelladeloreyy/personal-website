268f54cf2abb2d690ff10123d265d39d7f3ff94c
stelladeloreyy <deloreyste@gmail.com>
2025-09-07 18:24:29 -0400

    updated projects section to have base sticky notes

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -3,25 +3,52 @@ import React from "react";
 import './Projects.css';
 import './Projects.mobile.css';
 import NavBar from '../../components/navbar/NavBar'
-import ProjectsEmpty from '../../assets/images/projects-empty.png';
-import ProjectsEmptyFr from '../../assets/images/projects-empty-fr.png';
+import Footer from '../../components/footer/Footer';
+import StickyNote1 from '../../assets/images/sticky-note1.png';
+import StickyNote2 from '../../assets/images/sticky-note2.png';
+import StickyNote3 from '../../assets/images/sticky-note3.png';
+import StickyNote4 from '../../assets/images/sticky-note4.png';
+import Sparkles1 from '../../assets/images/corner-sparkle1.png';
+import Sparkles2 from '../../assets/images/corner-sparkle2.png';
+
 
 const Projects = ({ setLang, lang }) => {
     return (
         <div className="projects-container">
             <div className="header-sticky">
-                <NavBar section='projects' setLang={setLang} lang={lang} />
+                <NavBar section='project-notes' setLang={setLang} lang={lang} />
             </div>
-            <div className="projects-empty">
-                {
-                    lang === 'fr' ? (
-                        <img src={ProjectsEmptyFr} alt="Rien a voir ici (pour l'instant)" />
-                    )
-                    : 
-                    (
-                        <img src={ProjectsEmpty} alt="Nothing to see here (yet)" />
-                    )
-                }
+            <div className="projects-header">
+
+            </div>
+            <div className="project-notes">
+                <div className="project-notes-row">
+                    <div className="sticky-note">
+                        <img src={StickyNote1} alt="Sticky Note" />
+                        <img src={Sparkles1} className="show-grow1" alt="Sticky Note" />
+                        <img src={Sparkles2} className="show-grow2" alt="Sticky Note" />
+                    </div>
+                    <div className="sticky-note">
+                        <img src={StickyNote2} alt="Sticky Note" />
+                        <img src={Sparkles1} className="show-grow1" alt="Sticky Note" />
+                        <img src={Sparkles2} className="show-grow2" alt="Sticky Note" />
+                    </div>
+                </div>
+                <div className="project-notes-row">
+                    <div className="sticky-note">
+                        <img src={StickyNote3} alt="Sticky Note" />
+                        <img src={Sparkles1} className="show-grow1" alt="Sticky Note" />
+                        <img src={Sparkles2} className="show-grow2" alt="Sticky Note" />
+                    </div>
+                    <div className="sticky-note">
+                        <img src={StickyNote4} alt="Sticky Note" />
+                        <img src={Sparkles1} className="show-grow1" alt="Sticky Note" />
+                        <img src={Sparkles2} className="show-grow2" alt="Sticky Note" />
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <Footer lang={lang} />
             </div>
         </div>
     );
